2f444a036ed8c0efea4c8552f8522a511c513c76
PigGuilty <matteo.zotto@sfr.fr>
2018-03-01 23:34:24 +0100

    First Gun

---
+++ TMMFT Version n°2/Assets/Shoot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    // Use this for initialization
+    public float DegatArme;
+
+    public Camera fpsCam;
+    public ParticleSystem Tire;
+    public GameObject impactEffect;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            Tire.Play();
+
+            RaycastHit hit;
+            Ray ShootingDirection = new Ray(fpsCam.transform.position, fpsCam.transform.forward);
+
+            if (Physics.Raycast(ShootingDirection, out hit))
+            {
+                Debug.Log(hit.transform.name);
+
+                Target target = hit.transform.GetComponent<Target>();
+
+                if (hit.collider.tag == "Vache")
+                {
+                    target.TakeDamage(DegatArme);
+
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.AddForce(-hit.normal * DegatArme * 4);
+                    }
+                }
+
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
+
+            }
+        }
+    }
+}
---
+++ TMMFT Version n°2/Assets/Target.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float Vie;
+
+    public void TakeDamage (float amount)
+    {
+        Vie -= amount;
+        if(Vie <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+}
