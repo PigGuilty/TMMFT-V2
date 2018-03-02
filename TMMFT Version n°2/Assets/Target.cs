94b60551cc2d6daa8752c48464aaefbe87f3d8ca
PigGuilty <matteo.zotto@sfr.fr>
2018-03-02 11:53:49 +0100

    On est vraiment obligé de mettre un nom ?

--- TMMFT Version n°2/Assets/Target.cs
+++ TMMFT Version n°2/Assets/Target.cs
@@ -4,15 +4,37 @@ public class Target : MonoBehaviour {
 
     // Use this for initialization
 
+    public Color blue = new Color(0.2F, 0.0F, 1.0F, 1.0F);
+
     public float Vie;
 
     public void TakeDamage (float amount)
     {
         Vie -= amount;
+
         if(Vie <= 0)
         {
             Destroy(gameObject);
         }
+
+        else
+        {        
+            Material material = new Material(Shader.Find("Transparent/Diffuse"));
+            material.color = Color.red;
+            GetComponent<Renderer>().material = material;
+        }
+
     }
-	
+
+    public void Update()
+    {
+        Material material = new Material(Shader.Find("Transparent/Diffuse"));
+        if (material.color == Color.red)
+        {
+            material.color = Color.blue;
+            GetComponent<Renderer>().material = material;
+        }
+    }
+
 }
+	
