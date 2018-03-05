80b3d91c648673b3021bee0729bcd3d8d2b6adc3
EaglesMaster <serveur-gekhryso@outlook.com>
2018-03-05 16:46:49 +0100

    Script correct

--- TMMFT Version n°2/Assets/Controller/WeaponChange.cs
+++ TMMFT Version n°2/Assets/Controller/WeaponChange.cs
@@ -11,24 +11,24 @@ public class WeaponChange : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		pistolet.enabled = true;
+		pistolet.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Alpha3) | Input.GetKeyDown (KeyCode.Alpha4)) {
-			pistolet.enabled = false;
-			fusil.enabled = false;
-			mitrailleur.enabled = false;
+			pistolet.SetActive(false);
+			fusil.SetActive(false);
+			mitrailleur.SetActive(false);
 			//bazooka.enabled = false;
 		}
 
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			pistolet.enabled = true;
+			pistolet.SetActive(true);
 		}else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			fusil.enabled = true;
+			fusil.SetActive(true);
 		}else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			mitrailleur.enabled = true;
+			mitrailleur.SetActive(true);
 		}/* else if (Input.GetKeyDown (KeyCode.Alpha4)) {
 			bazooka.enabled = true;
 		}*/
