ae4fb019ed2c3f68d5e1b249fff1c431b58931ad
EaglesMaster <serveur-gekhryso@outlook.com>
2018-03-06 16:13:15 +0100

    Corrections

---
+++ TMMFT Version n°2/Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public int SpawnSpeed;
+	public GameObject EnnemiPrefab;
+	private int Counter;
+
+	// Use this for initialization
+	void Start () {
+		Counter = SpawnSpeed+1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Counter--;
+
+		if (Counter <= 0) {
+			Counter = SpawnSpeed;
+
+			Instantiate(EnnemiPrefab, transform);
+		}
+	}
+}
