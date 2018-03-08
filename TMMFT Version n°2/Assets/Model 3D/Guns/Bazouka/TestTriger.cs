eff191d4917ff59c74beea72a5bc55c2cb8d7d57
PigGuilty <matteo.zotto@sfr.fr>
2018-03-08 11:45:22 +0100

    test bazooka : echec :-1:

---
+++ TMMFT Version n°2/Assets/Model 3D/Guns/Bazouka/TestTriger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTriger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+         public float radius = 5.0F;
+     public float power = 10.0F;
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+           
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.
+    }
+}
