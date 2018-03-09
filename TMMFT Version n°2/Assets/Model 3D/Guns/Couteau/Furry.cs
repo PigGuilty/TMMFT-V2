52076db5de045770e58b808c344f9841a04caf98
PigGuilty <matteo.zotto@sfr.fr>
2018-03-09 18:00:23 +0100

    animation fini !! (couteau baguette)

--- TMMFT Version n°2/Assets/Model 3D/Guns/Couteau/Furry.cs
+++ TMMFT Version n°2/Assets/Model 3D/Guns/Couteau/Furry.cs
@@ -7,6 +7,15 @@ public class Furry : MonoBehaviour {
     private Attaque attaque;
     private Baguette baguette;
 
+    private Animator animator;
+
+    private int AnimationLengthCouteau;
+    private int AnimationLengthBaguette;
+    private int AnimationWaitEnd;
+
+    private bool AnimCouteauVersBaguette;
+    private bool AnimBaguetteVersCouteau;
+
     private void Awake()
     {
         attaque = GetComponent<Attaque>();
@@ -15,15 +24,73 @@ public class Furry : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
+        animator = GetComponent<Animator>();
+        AnimCouteauVersBaguette = false;
+        AnimBaguetteVersCouteau = false;
+        AnimationLengthCouteau = 162;
+        AnimationLengthBaguette = 200;
+        AnimationWaitEnd = 0;
+        baguette.enabled = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (attaque.furry >= 100)
         {
             attaque.enabled = false;
-            baguette.enabled = true;
+            AnimCouteauVersBaguette = true;
+            if (AnimCouteauVersBaguette == true)
+            {
+                if (AnimationWaitEnd == 0)
+                {
+                    animator.Rebind();
+                    animator.SetFloat("Attaque Couteau", 0.5f);
+                }
+
+                AnimationWaitEnd = AnimationWaitEnd + 1;
+
+                //attendre fin de l'annimation
+                if (AnimationWaitEnd >= AnimationLengthCouteau)
+                {
+                    animator.SetFloat("Attaque Couteau", 0.75f);
+                    AnimationWaitEnd = 0;
+                    baguette.enabled = true;
+                    AnimCouteauVersBaguette = false;
+                    attaque.furry = 99;
+                }
+
+            }
         }
-	}
+        if (attaque.furry <= -1)
+        {
+            baguette.enabled = false;
+            AnimBaguetteVersCouteau = true;
+            if (AnimBaguetteVersCouteau == true)
+            {
+                if (AnimationWaitEnd == 0)
+                {
+                    animator.Rebind();
+                    animator.SetFloat("Attaque Couteau", 1f);
+                }
+
+                AnimationWaitEnd = AnimationWaitEnd + 1;
+
+                //attendre fin de l'annimation
+                if (AnimationWaitEnd >= AnimationLengthBaguette)
+                {
+                    animator.SetFloat("Attaque Couteau", 0f);
+                    AnimationWaitEnd = 0;
+                    attaque.enabled = true;
+                    AnimBaguetteVersCouteau = false;
+                    attaque.furry = 0;
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            attaque.furry = attaque.furry - 5;
+        }
+     }
 }
