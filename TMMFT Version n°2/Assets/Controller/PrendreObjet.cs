2a157c2567694f1a57cf6307302395b8a6779e60
PigGuilty <matteo.zotto@sfr.fr>
2018-04-01 19:28:09 +0200

    Prendre Objet
    La lanterne à huile a été modélisé, j'ai mis des matérials, une point light et un box collider
    ensuite j'ai fait un script pour prendre l'objet avec le bouton "e"

---
+++ TMMFT Version n°2/Assets/Controller/PrendreObjet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrendreObjet : MonoBehaviour
+{
+
+    public float PortéePourPrendreObjet;
+
+    public GameObject PlacePourObjet;
+
+    public GameObject pistolet;
+    public GameObject fusil;
+    public GameObject mitrailleur1;
+    public GameObject mitrailleur2;
+    public GameObject bazooka;
+    public GameObject Couteau;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ShootingDirection = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            if (Physics.Raycast(ShootingDirection, out hit, PortéePourPrendreObjet))
+            {
+                Transform PositionObjet = hit.transform.GetComponent<Transform>();
+                Rigidbody rigidbody = hit.transform.GetComponent<Rigidbody>();
+
+                if (hit.collider.tag == "Objet")
+                {
+                    pistolet.SetActive(false);
+                    fusil.SetActive(false);
+                    mitrailleur1.SetActive(false);
+                    mitrailleur2.SetActive(false);
+                    bazooka.SetActive(false);
+                    Couteau.SetActive(false);
+                    PositionObjet.transform.rotation = PlacePourObjet.transform.rotation;
+                    PositionObjet.transform.position = PlacePourObjet.transform.position;
+                    rigidbody.isKinematic = true;
+                    PositionObjet.transform.parent = PlacePourObjet.transform;
+                }
+            }
+        }
+    }
+}
