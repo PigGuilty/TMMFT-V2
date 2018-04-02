39627ce0de206c33c5e5e546a7cab91e43a37a61
PigGuilty <matteo.zotto@sfr.fr>
2018-04-02 11:52:35 +0200

    Jetter l'objet

--- TMMFT Version n°2/Assets/Controller/PrendreObjet.cs
+++ TMMFT Version n°2/Assets/Controller/PrendreObjet.cs
@@ -18,6 +18,9 @@ public class PrendreObjet : MonoBehaviour
 
     private bool ObjetPris;
 
+    private string ObjectName;
+    private GameObject ObjectQuiEstPris;
+
     // Use this for initialization
     void Start()
     {
@@ -35,24 +38,45 @@ public class PrendreObjet : MonoBehaviour
 
             if (Physics.Raycast(ShootingDirection, out hit, PortéePourPrendreObjet))
             {
+                ObjectName = hit.collider.gameObject.name;
+
                 Transform PositionObjet = hit.transform.GetComponent<Transform>();
                 Rigidbody rigidbody = hit.transform.GetComponent<Rigidbody>();
 
                 if (hit.collider.tag == "Objet")
                 {
                     ObjetPris = true;
+
                     pistolet.SetActive(false);
                     fusil.SetActive(false);
                     mitrailleur1.SetActive(false);
                     mitrailleur2.SetActive(false);
                     bazooka.SetActive(false);
                     Couteau.SetActive(false);
+
                     PositionObjet.transform.rotation = PlacePourObjet.transform.rotation;
                     PositionObjet.transform.position = PlacePourObjet.transform.position;
                     rigidbody.isKinematic = true;
                     PositionObjet.transform.parent = PlacePourObjet.transform;
                 }
             }
+        }
+
+        if (Input.GetButtonDown("Fire1") && ObjetPris == true)
+        {
+            //ajouter une force au rigidbody de l'objet... mais on n'y a plus accès :/
+            print(ObjectName);
+            ObjectQuiEstPris = GameObject.Find(ObjectName);
+            Rigidbody rigidbodyDeLObjetPris = ObjectQuiEstPris.transform.GetComponent<Rigidbody>();
+            Vector3 DirectionForceJet = Camera.main.transform.forward + new Vector3(0f, 0.4f, 0f);
+
+            Debug.DrawRay(transform.position, DirectionForceJet *4, Color.green);
+
+            rigidbodyDeLObjetPris.isKinematic = false;
+            ObjectQuiEstPris.transform.parent = null;
+            rigidbodyDeLObjetPris.AddForce(DirectionForceJet * 500);
+
+            ObjetPris = false;
         }
     }
 }
