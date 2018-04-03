d81df2bf6f83999682daabd9e1d34c470446f205
PigGuilty <matteo.zotto@sfr.fr>
2018-04-03 19:43:34 +0200

    Grenade

---
+++ TMMFT Version n°2/Assets/Model 3D/Guns/ObjetThrowable/Grenade/EffetGrenade.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffetGrenade : MonoBehaviour {
+
+    public GameObject DemiSphère;
+    public GameObject Sphère;
+
+    public GameObject grenade;
+
+    public bool canExplode;
+
+    // Use this for initialization
+    void start () {
+        canExplode = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canExplode = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (canExplode == true)
+        {
+            if (collision.gameObject.tag != "Player")
+            {
+                if (collision.gameObject.tag == "Vache")
+                {
+                    Instantiate(Sphère, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
+                }
+                else
+                {
+                    Instantiate(DemiSphère, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+}
