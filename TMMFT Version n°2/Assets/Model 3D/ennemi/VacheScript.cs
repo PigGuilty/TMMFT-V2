280a43c54de2d08caf6614f13f84ae08ebd6207f
PigGuilty <matteo.zotto@sfr.fr>
2018-04-04 19:27:50 +0200

    Vache Modif (Are you my mummy ?)

--- TMMFT Version n°2/Assets/Model 3D/ennemi/VacheScript.cs
+++ TMMFT Version n°2/Assets/Model 3D/ennemi/VacheScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class VacheScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Rigidbody rb;
+
+    public float Speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        Vector3 Newpos = Camera.main.transform.position - Vector3.up *3;
+
+        Vector3 PlayerDirection = Newpos - transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(PlayerDirection);
+        transform.rotation = rotation;
+
+        rb.AddForce(PlayerDirection * Speed);
+
+    }
 }
