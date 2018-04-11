4c34b11a5be7079bef5d506c16a377790b1c96b7
PigGuilty <matteo.zotto@sfr.fr>
2018-04-11 22:17:31 +0200

    Début d'armoire

---
+++ TMMFT Version n°2/Assets/Model 3D/Décors interractif/Armoire/ArmoireScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoireScript : MonoBehaviour {
+
+    public GameObject Player;
+
+    public GameObject SpawnTéléport;
+
+    private bool Ouvert;
+    private bool AnimationOuverture;
+
+    private Animator animator;
+    PortailUtilisable PortailUtilisablee;
+
+    private int AnimationLengthOuverture;
+    private int AnimationWaitEndOuverture;
+
+    private int TempsPortailReload;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        PortailUtilisablee = Player.GetComponent<PortailUtilisable>();
+
+        AnimationOuverture = false;
+        Ouvert = false;
+
+        AnimationLengthOuverture = 50;
+        AnimationWaitEndOuverture = 0;
+
+        animator.SetFloat("Armoire", 0.0f);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (AnimationOuverture == true)
+        {
+            if (AnimationWaitEndOuverture == 0)
+            {
+                animator.Rebind();
+                animator.SetFloat("Armoire", 0.66f);
+            }
+
+            AnimationWaitEndOuverture = AnimationWaitEndOuverture + 1;
+
+            if (AnimationWaitEndOuverture >= AnimationLengthOuverture)
+            {
+                Ouvert = true;
+                AnimationOuverture = false;
+                animator.SetFloat("Armoire", 1.0f);
+            }
+        }
+
+    }
+
+
+
+    public void HitByRaycast()
+    {
+        if (Ouvert == false)
+        {
+            AnimationOuverture = true;
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Ouvert == true && PortailUtilisablee.PortailUtilisablee == true && (other.tag == "Player" || other.tag == "Objet"))
+        {
+            PortailUtilisablee.PortailUtilisablee = false;
+            TempsPortailReload = 0;
+            other.transform.position = SpawnTéléport.transform.position;
+        }
+    }
+
+
+}
---
+++ TMMFT Version n°2/Assets/Model 3D/Décors interractif/Armoire/PortailUtilisable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortailUtilisable : MonoBehaviour {
+
+    public bool PortailUtilisablee;
+    private int TempsPortailReload;
+
+    // Use this for initialization
+    void Start () {
+        PortailUtilisablee = true;
+        TempsPortailReload = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(PortailUtilisablee == false)
+        {
+            if (TempsPortailReload >= 20)
+            {
+                PortailUtilisablee = true;
+                TempsPortailReload = 0;
+            }
+            TempsPortailReload++;
+        }
+
+    }
+}
