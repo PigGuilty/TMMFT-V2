5da0404374a4af281179f5a19ad832bf3110be50
PigGuilty <matteo.zotto@sfr.fr>
2018-04-15 17:51:19 +0200

    Changement d'arme
    On peut changer d'arme avec la roulette de la souris

--- TMMFT Version n°2/Assets/Controller/WeaponChange.cs
+++ TMMFT Version n°2/Assets/Controller/WeaponChange.cs
@@ -11,39 +11,90 @@ public class WeaponChange : MonoBehaviour {
     public GameObject bazooka;
     public GameObject Couteau;
 
-	// Use this for initialization
-	void Start () {
-		pistolet.SetActive(true);
-		bazooka.SetActive(false);
-		fusil.SetActive(false);
-		mitrailleur1.SetActive(false);
-		mitrailleur2.SetActive(false);
-        Couteau.SetActive(false);
+    private int IDWeaponDemandé;
+    private bool DemandeChangementArme;
 
+    // Use this for initialization
+    void Start () {
+        IDWeaponDemandé = 1;
+        DemandeChangementArme = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Alpha3) || Input.GetKeyDown (KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Alpha5)) {
-			pistolet.SetActive(false);
-			fusil.SetActive(false);
-			mitrailleur1.SetActive(false);
-			mitrailleur2.SetActive(false);
-			bazooka.SetActive(false);
-            Couteau.SetActive(false);
+
+        /****Début Changement D'arme par Clavier****/
+
+        if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Alpha3) || Input.GetKeyDown (KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Alpha5)) {
+            DemandeChangementArme = true;
         }
 
-		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			pistolet.SetActive(true);
-		}else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			fusil.SetActive(true);
-		}else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			mitrailleur1.SetActive(true);
-			mitrailleur2.SetActive(true);
-		}else if (Input.GetKeyDown (KeyCode.Alpha4)) {
-			bazooka.SetActive(true);
-        }else if (Input.GetKeyDown (KeyCode.Alpha5)) {
-			Couteau.SetActive(true);
-		}
+        if (Input.GetKeyDown (KeyCode.Alpha1)) {
+            IDWeaponDemandé = 1;
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+            IDWeaponDemandé = 2;
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+            IDWeaponDemandé = 3;
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+            IDWeaponDemandé = 4;
+        }
+        else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+            IDWeaponDemandé = 5;
+        }
+
+        /****Fin Changement D'arme par Clavier****/
+
+        /****Début Changement D'arme par Souris****/
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && IDWeaponDemandé < 5)
+        {
+            IDWeaponDemandé++;
+            DemandeChangementArme = true;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && IDWeaponDemandé > 1)
+        {
+            IDWeaponDemandé--;
+            DemandeChangementArme = true;
+        }
+
+        /****Fin Changement D'arme par Souris****/
+
+
+        if (DemandeChangementArme == true)
+        {
+            pistolet.SetActive(false);
+            fusil.SetActive(false);
+            mitrailleur1.SetActive(false);
+            mitrailleur2.SetActive(false);
+            bazooka.SetActive(false);
+            Couteau.SetActive(false);
+
+            if (IDWeaponDemandé == 1)
+            {
+                pistolet.SetActive(true);
+            }
+            else if (IDWeaponDemandé == 2)
+            {
+                fusil.SetActive(true);
+            }
+            else if (IDWeaponDemandé == 3)
+            {
+                mitrailleur1.SetActive(true);
+                mitrailleur2.SetActive(true);
+            }
+            else if (IDWeaponDemandé == 4)
+            {
+                bazooka.SetActive(true);
+            }
+            else if (IDWeaponDemandé == 5)
+            {
+                Couteau.SetActive(true);
+            }
+            DemandeChangementArme = false;
+        }
 	}
 }
