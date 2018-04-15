56b3eba6b43f0b9c4b512eca1e25f3c65fa59e29
PigGuilty <matteo.zotto@sfr.fr>
2018-04-15 17:18:24 +0200

    Disparition des débrits
    Solution efficace pour optimiser un peu le jeu

--- TMMFT Version n°2/Assets/Model 3D/UseLess Throwable/AddforceAssièteCassé.cs
+++ TMMFT Version n°2/Assets/Model 3D/UseLess Throwable/AddforceAssièteCassé.cs
@@ -6,11 +6,26 @@ public class AddforceAssièteCassé : MonoBehaviour {
 
     public assièteBreking assièteBreking;
 
-	// Use this for initialization
-	void Start () {
+    private int RandomNumber;
+    private int increase;
+
+    // Use this for initialization
+    void Start () {
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         assièteBreking.GetComponent<assièteBreking>();
         print("assiète" + assièteBreking.dir);
         rb.AddForce(assièteBreking.dir * 500);
-	}
+
+        RandomNumber = Random.Range(700, 740);
+    }
+
+    private void Update()
+    {
+        if(increase >= RandomNumber)
+        {
+            Destroy(gameObject);
+        }
+
+        increase++;
+    }
 }
