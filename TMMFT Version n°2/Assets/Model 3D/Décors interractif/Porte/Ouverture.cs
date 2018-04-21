b1332c9fdf755f7eb1d25d10a69776ae20a763c0
PigGuilty <matteo.zotto@sfr.fr>
2018-04-21 11:58:10 +0200

    Bug corrigé
    La porte doit attendre la fin de sa séquence d'animation pour en commencer une autre

--- TMMFT Version n°2/Assets/Model 3D/Décors interractif/Porte/Ouverture.cs
+++ TMMFT Version n°2/Assets/Model 3D/Décors interractif/Porte/Ouverture.cs
@@ -6,9 +6,6 @@ public class Ouverture : MonoBehaviour {
 
     private Animator animator;
 
-    private int AnimationLength;
-    private int AnimationWaitEnd;
-
     private float FloatDeLAnimation;
 
     public float FloatDeDépartPourAnimation;
@@ -16,38 +13,65 @@ public class Ouverture : MonoBehaviour {
     public BoxCollider boxOuvert;
     public BoxCollider boxFermé;
 
+    private bool EnAnimation;
+
+    private int AnimationWaitEnd;
+    private int AnimationLength;
+
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
-
-        AnimationLength = 30;
-        AnimationWaitEnd = 0;
 
         animator.SetFloat("Ouverture", FloatDeDépartPourAnimation);
 
         boxOuvert.enabled = false;
         boxFermé.enabled = true;
+
+        EnAnimation = false;
+        AnimationWaitEnd = 0;
+        AnimationLength = 30;
     }
 
     public void HitByRaycast()
     {
-        FloatDeLAnimation = animator.GetFloat("Ouverture");
-
-        if (FloatDeLAnimation == 1f)
+        if (EnAnimation == false)
         {
+
+            FloatDeLAnimation = animator.GetFloat("Ouverture");
+
+            EnAnimation = true;
+
+            if (FloatDeLAnimation == 1f)
+            {
                 animator.Rebind();
                 animator.SetFloat("Ouverture", 0.33f);
+
                 boxOuvert.enabled = false;
                 boxFermé.enabled = true;
-        }
+            }
 
-        if (FloatDeLAnimation == 0.33f)
-        {
+            if (FloatDeLAnimation == 0.33f)
+            {
                 animator.Rebind();
                 animator.SetFloat("Ouverture", 1f);
+
                 boxOuvert.enabled = true;
                 boxFermé.enabled = false;
+            }
         }
+    }
 
+    private void Update()
+    {
+        if(EnAnimation == true)
+        {
+            AnimationWaitEnd = AnimationWaitEnd + 1;
+
+            if (AnimationWaitEnd >= AnimationLength)
+            {
+                AnimationWaitEnd = 0;
+                EnAnimation = false;
+            }
+        }
     }
 }
