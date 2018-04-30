bc325debb39539d36fbbb894e9cfafd38c5b1b05
PigGuilty <matteo.zotto@sfr.fr>
2018-04-30 18:24:04 +0200

    SCHTONK !
    Des objets font schtonk en touchant quelque chose

---
+++ TMMFT Version n°2/Assets/feedback/Sonor/SchtonkTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SchtonkTrigger : MonoBehaviour
+{
+    public AudioClip schtonk;
+    private AudioSource Audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "Vache")
+        {
+            Audio = gameObject.GetComponent<AudioSource>();
+            Audio.clip = schtonk;
+            Audio.Play();
+        }
+    }
+}
