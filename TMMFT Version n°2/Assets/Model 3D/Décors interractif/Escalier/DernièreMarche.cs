2e9cfe8872184e1a0812ddefb5dc72b01e995db8
PigGuilty <matteo.zotto@sfr.fr>
2018-05-02 19:38:12 +0200

    Bouton et escalier relié

--- TMMFT Version n°2/Assets/Model 3D/Décors interractif/Escalier/DernièreMarche.cs
+++ TMMFT Version n°2/Assets/Model 3D/Décors interractif/Escalier/DernièreMarche.cs
@@ -7,21 +7,40 @@ public class DernièreMarche : MonoBehaviour
 
     private Animator animator;
 
+    public GameObject Cube;
+    public GameObject CubeTigger;
+
+    private BoxCollider CubeBox;
+    private BoxCollider CubeTriggerBox;
+
+    public PhysicMaterial mat;
     // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        CubeBox = Cube.GetComponent<BoxCollider>();
+        CubeTriggerBox = CubeTigger.GetComponent<BoxCollider>();
+
+        CubeTriggerBox.enabled = false;
+        CubeBox.material = null;
     }
 
     public void Appuyé()
     {
         animator.Rebind();
         animator.SetFloat("Escalier", 1);
+
+        CubeTriggerBox.enabled = true;
+        CubeBox.material = mat;
     }
 
     public void Relaché()
     {
         animator.Rebind();
         animator.SetFloat("Escalier", 0);
+
+        CubeTriggerBox.enabled = false;
+        CubeBox.material = null;
     }
 }
