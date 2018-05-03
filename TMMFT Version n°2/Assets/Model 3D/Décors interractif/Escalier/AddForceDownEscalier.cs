deb56277ce3e9e5a58235d17466303d4360604c9
PigGuilty <matteo.zotto@sfr.fr>
2018-05-03 12:45:21 +0200

    GO GO GO

--- TMMFT Version n°2/Assets/Model 3D/Décors interractif/Escalier/AddForceDownEscalier.cs
+++ TMMFT Version n°2/Assets/Model 3D/Décors interractif/Escalier/AddForceDownEscalier.cs
@@ -1,51 +1,89 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-
-public class AddForceDownEscalier : MonoBehaviour {
-
-    private Rigidbody rb;
-
-    public float x;
-    public float y;
-    public float z;
-    public float speed;
-
-  
-/*    void Start () {
-		
-	}
-	
-
-	void Update () {
-		
-	}*/
-
-    private void OnTriggerEnter(Collider other)
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using Random = UnityEngine.Random;
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class AddForceDownEscalier : MonoBehaviour
     {
-        rb = other.GetComponent<Rigidbody>();
-        if (other.gameObject.tag == "Player")
+
+        private Rigidbody rb;
+
+        public float x;
+        public float y;
+        public float z;
+
+        public int speedWalk;
+        public int speedRun;
+        private int increase;
+
+        private Vector3 dir;
+
+        private FirstPersonController fps;
+
+        private bool redonnerKinematic;
+
+        void Start()
         {
-            rb.isKinematic = false;
-            rb.AddForce(new Vector3(x, y, z) * speed);
+            dir = new Vector3(x, y, z);
+            increase = 0;
         }
-    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        rb = other.GetComponent<Rigidbody>();
-        if (other.gameObject.tag == "Player")
+
+
+/*        void Update()
         {
-            rb.AddForce(new Vector3(x, y, z) * speed);
+            if(redonnerKinematic == true)
+            {
+                if(increase>= 20)
+                {
+                    rb.isKinematic = true;
+                    increase = 0;
+                    redonnerKinematic = false;
+                }
+                increase++;
+            }
+        }*/
+
+        private void OnTriggerEnter(Collider other)
+        {
+            rb = other.GetComponent<Rigidbody>();
+
+            if (other.gameObject.tag == "Player")
+            {               
+                rb.isKinematic = false;
+                rb.AddForce(dir * speedWalk);
+            }
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
+        private void OnTriggerStay(Collider other)
         {
-            rb.AddForce(new Vector3(x, -y, z) * speed * 200);
-            rb.isKinematic = true;
+
+            rb = other.GetComponent<Rigidbody>();
+            fps = other.GetComponent<FirstPersonController>();
+
+            if (other.gameObject.tag == "Player")
+            {
+                if(fps.m_IsWalking == true)
+                {
+                    rb.AddForce(dir * speedWalk);
+                }
+                else
+                {
+                    rb.AddForce(dir * speedRun);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                rb.AddForce(new Vector3(x, -y, z) * speedWalk * 200);
+                //redonnerKinematic = true;
+                rb.isKinematic = true;
+            }
         }
     }
 }
