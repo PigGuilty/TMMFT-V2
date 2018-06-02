9c9047e279e6eb5c7d39f01cc3ba22ed4f4737da
PigGuilty <matteo.zotto@sfr.fr>
2018-06-02 21:51:58 +0200

    Mise à l'écart

--- TMMFT Version n°2/Assets/Model 3D/Décors/Meuble/ObjetDansLeTiroir.cs
+++ TMMFT Version n°2/Assets/Model 3D/Décors/Meuble/ObjetDansLeTiroir.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class ObjetDansLeTiroir : MonoBehaviour {
 
-	public bool ObjectDansLeTriroirDroit;
+	public bool TiroirDroit;
 
-	public Vector3 PosInitial;
-	public Vector3 PosFinal;
+	private Vector3 PosInitial;
+	private Vector3 PosFinal;
 
 	public GameObject Meuble;
 
 	private OuvrirTiroir ouvrirTiroir;
 
+	public float variationX = -0.285f;
+	public float variationY = +0.285f;
+
 	// Use this for initialization
 	void Start () {
 		ouvrirTiroir = Meuble.GetComponent<OuvrirTiroir> ();
+
+		PosInitial = gameObject.transform.position;
+		PosFinal = new Vector3 (gameObject.transform.position.x + variationX, gameObject.transform.position.y, gameObject.transform.position.z + variationY);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (ouvrirTiroir.EnAnimationDroit == true && ObjectDansLeTriroirDroit == true) {
+		if (ouvrirTiroir.EnAnimationDroit == true && TiroirDroit == true) {
 			Vector3 newPos = ((PosFinal - PosInitial) * ouvrirTiroir.keyDroite / 100) + PosInitial;
 			gameObject.transform.position = newPos;
 		}
 
-		if (ouvrirTiroir.EnAnimationGauche == true && ObjectDansLeTriroirDroit == false) {
+		if (ouvrirTiroir.EnAnimationGauche == true && TiroirDroit == false) {
 			Vector3 newPos = ((PosFinal - PosInitial) * ouvrirTiroir.keyGauche / 100) + PosInitial;
 			gameObject.transform.position = newPos;
 		}
