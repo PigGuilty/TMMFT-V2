8a67ec3978ebc92d4226f69fb420076ffffe29e8
PigGuilty <matteo.zotto@sfr.fr>
2018-06-22 18:18:55 +0200

    Rotation Tête Vache V3 Lowpoly

---
+++ TMMFT Version n°2/Assets/Model 3D/ennemi/Bipède/RotationDeLaTete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationDeLaTete : MonoBehaviour {
+
+	private Camera player;
+	public GameObject Root_HeadBone;
+	private Vector3 TargetPoint;
+
+	// Use this for initialization
+	void Start () {
+		player = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TargetPoint = new Vector3 (player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+		Root_HeadBone.transform.LookAt (player.transform.position ,Vector3.up);
+	}
+}
