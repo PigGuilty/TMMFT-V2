a0fb6c8ebfa81d2680cf8512e587ee12106ab069
PigGuilty <matteo.zotto@sfr.fr>
2018-09-23 22:51:24 +0200

    Grekoj
    Look behind you

---
+++ TMMFT Version n°2/Assets/Model 3D/UseLess Throwable/Chess/KingsDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingsDeath : MonoBehaviour {
+
+	public GameObject Tresor;
+	public GameObject kingsdeath;
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.transform.tag == "Bullet") {
+			Tresor.SetActive (true);
+			Destroy (kingsdeath.GetComponent<KingsDeath> ());
+			Destroy (kingsdeath.GetComponent<Rigidbody> ());
+		}
+	}
+}
